ce7461155f19d18e25861e9770471309972a2b9d
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2019-12-19 12:51:40 +0200

    erever sent messages beetwen different cients

--- server1.py
+++ server1.py
@@ -5,17 +5,31 @@ host = socket.gethostname()
 port = 12345
 s.bind((host, port))
 
+
+def message(s):
+    l = s.split(':')[0]
+    mes = s.split(':')[1]
+    if l == 'Dan':
+        c2.send(mes)
+    if l == 'Alice':
+        c1.send(mes)
+
+
+
+
 s.listen(5)
 while True:
    c1, addr = s.accept()
    c2, addr = s.accept()
    print ('Получил соединение от', addr)
-   c1.send(bytes('Hello, I am a server Bob. I will help you to speak with your friends. So, lets go to speak!','utf8'))
-   c2.send(bytes('Hello, I am a server Bob. I will help you to speak with your friends. So, lets go to speak!','utf8'))
-  # print(c1.recv(1024))
-  # print(c2.recv(1024))
-   c1.send(c2.recv(1024))
-   c2.send(c1.recv(1024))
+   c1.send(bytes('Hi','utf8'))
+   c2.send(bytes('Hi','utf8'))
+   # print(c1.recv(1024))
+   # print(c2.recv(1024))
+   # c1.send(c2.recv(1024))
+   # c2.send(c1.recv(1024))
+   message(c1.recv(1024))
+   message(c2.recv(1024))
    c1.close()
    c2.close()
 
