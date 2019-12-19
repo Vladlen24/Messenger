73fb2fbcad8650357bc776afae71a02564fb8083
Vladlen Statiev <vladlenstatiev20022405@gmail.com>
2019-12-19 12:11:30 +0200

    server sent messeges from different clients (but they are the same)

--- server1.py
+++ server1.py
@@ -5,12 +5,18 @@ host = socket.gethostname()
 port = 12345
 s.bind((host, port))
 
-print(host)
 s.listen(5)
 while True:
-   c, addr = s.accept()    
+   c1, addr = s.accept()
+   c2, addr = s.accept()
    print ('Получил соединение от', addr)
-   c.send(bytes('Hello','utf8'))
-   print(c.recv(1024))
-   c.close()
+   c1.send(bytes('Hello, I am a server Bob. I will help you to speak with your friends. So, lets go to speak!','utf8'))
+   c2.send(bytes('Hello, I am a server Bob. I will help you to speak with your friends. So, lets go to speak!','utf8'))
+  # print(c1.recv(1024))
+  # print(c2.recv(1024))
+   c1.send(c2.recv(1024))
+   c2.send(c1.recv(1024))
+   c1.close()
+   c2.close()
+
 
